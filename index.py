83f8c41cc1f76444587a0157e912253fff5273be
str-okada <shotarookadaa@gmail.com>
2023-02-28 18:21:26 -0800

    sending img and time been added

--- index.py
+++ index.py
@@ -1,6 +1,11 @@
 import requests 
 import os
 from dotenv import load_dotenv
+import datetime
+import pytz
+
+time = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+time = time.strftime('%m/%d/%Y, %H:%M:%S')
 
 # .envファイルの内容を読み込見込む
 load_dotenv()
@@ -10,16 +15,16 @@ load_dotenv()
 
 TOKEN = os.environ['API_KEY']
 api_url = 'https://notify-api.line.me/api/notify'
-send_contens = 'Taro'
+send_contens = time
 
 TOKEN_dic = {'Authorization' : 'Bearer' + ' ' + TOKEN}
 send_dic = {'message' : send_contens}
 
 # print(TOKEN_dic)
 # print(send_dic)
-# requests.post(api_url, headers=TOKEN_dic, data=send_dic)
+requests.post(api_url, headers=TOKEN_dic, data=send_dic)
 
-image_file = './students.png'
+image_file = './img/cat.png'
 binary = open(image_file, mode="rb")
 image_dic = {'imageFile':binary}
 
